d9f9a5d8d0a1518983802d10d66488d139a0229e
lislisowni2 <rafalkozikowski735@gmail.com>
2026-01-10 08:55:50 +0100

    Repair another task

--- tests/Feature/TDD/DownloadTest.php
+++ tests/Feature/TDD/DownloadTest.php
@@ -82,7 +82,7 @@ it('failed download from private by guest', function () {
         'path' => 'test.jpg'
     ]);
 
-    $response->assertStatus(200);
+    $response->assertStatus(403);
 });
 
 it('empty filename download', function () {
